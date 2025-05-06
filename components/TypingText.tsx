411b8ba2e23fc5707d7c76be4e33f2adb09edab7
John Murphy <chiefsmurph@gmail.com>
2025-05-05 23:28:32 -0700

    typing slow

--- components/TypingText.tsx
+++ components/TypingText.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 export const TypingText = ({
   text,
-  speed = 60,
+  speed = 70,
 }: {
   text: string;
   speed?: number;
@@ -11,17 +11,20 @@ export const TypingText = ({
   const [completed, setCompleted] = useState(false);
   useEffect(() => {
     let index = 0;
-    const interval = setInterval(() => {
-      setDisplayedText((prev) => prev + text.charAt(index));
-      index++;
-      if (index === text.length) {
-        clearInterval(interval);
-        setCompleted(true);
-      }
-    }, speed);
+    let interval;
+    // setTimeout(() => {
+      interval = setInterval(() => {
+        setDisplayedText((prev) => prev + text.charAt(index));
+        index++;
+        if (index === text.length) {
+          clearInterval(interval);
+          setCompleted(true);
+        }
+      }, speed);
+    // }, 3000);
 
     return () => clearInterval(interval);
-  }, [text, speed]);
+  }, []);
 
   return (
     <>
