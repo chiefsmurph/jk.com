0fa8c039d16001e0242154f121a27b2176818f66
John Murphy <chiefsmurph@gmail.com>
2025-07-04 15:49:43 -0400

    meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import { Analytics } from "@/components/Analytics";
 import "./globals.css";
 
 export const metadata = {
-  title: "Johnny Keeys - actor, musician",
-  description: "Escape the Ordinary—Own a Collectible Piece of Paradise.",
+  title: "Johnny Keeys - actor, model, musician",
+  description: "Dynamic, screen-tested performer with leading-role experience in TV and film.",
   icons: {
     icon: "/favicon/favicon.ico",
     apple: "/favicon/apple-icon.png",
